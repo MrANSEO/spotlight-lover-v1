9b614d6cd23f76ade127faccf9aae9f41e171064
MrANSEO <mranseo930@gmail.com>
2026-04-10 07:54:46 +0000

    page du vote X

--- backend/src/votes/votes.module.ts
+++ backend/src/votes/votes.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
-import { WebhooksController } from '../webhooks/webhooks.controller';
-import { WebhooksService } from '../webhooks/webhooks.service';
+import { VotesController } from './votes.controller';
+import { VotesService } from './votes.service';
 import { PaymentModule } from '../payments/payment.module';
 import { PrismaService } from '../prisma.service';
-import { ReferralModule } from '../referral/referral.module'; // ← ajoute cet import
 
 @Module({
-  imports: [
-    PaymentModule,
-    ReferralModule, // ← ajoute cette ligne
-  ],
-  controllers: [WebhooksController],
-  providers: [WebhooksService, PrismaService],
+  imports: [PaymentModule],
+  controllers: [VotesController],
+  providers: [VotesService, PrismaService],
 })
-export class WebhooksModule {}
+export class VotesModule {}
